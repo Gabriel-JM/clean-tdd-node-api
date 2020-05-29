cbbc9a245bc52b7a6726ffd2dc52fba2fe4bb4d2
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-05-29 13:52:04 -0300

    Ensure LoginRouter returns a valid token when valid credentials are provided

--- src/presentation/routers/login-router.spec.js
+++ src/presentation/routers/login-router.spec.js
@@ -86,7 +86,7 @@ describe('Login Router', () => {
   })
 
   test('should return 200 when valid credentials are provided', () => {
-    const { sut } = makeSut()
+    const { sut, authUseCaseSpy } = makeSut()
     const httpRequest = {
       body: {
         email: 'valid_email@email.com',
@@ -95,6 +95,7 @@ describe('Login Router', () => {
     }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body.accessToken).toEqual(authUseCaseSpy.accessToken)
   })
 
   test('should return 500 if no AuthUseCase is provided', () => {
