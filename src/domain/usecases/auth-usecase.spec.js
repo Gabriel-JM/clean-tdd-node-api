85ca37c454cf436f2de9de052b26d3da5ddf8c96
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-06-06 21:22:16 -0300

    Move sut creation to a factory helper method

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -1,29 +1,58 @@
 const { MissingParamError } = require('../../utils/errors')
 
-class AuthUseCase {
-  async auth (email, password) {
-    if (!email) {
-      throw new MissingParamError('email')
+const makeSut = () => {
+  class AuthUseCase {
+    constructor (loadUserByEmailRepository) {
+      this.loadUserByEmailRepository = loadUserByEmailRepository
     }
 
-    if (!password) {
-      throw new MissingParamError('password')
+    async auth (email, password) {
+      if (!email) {
+        throw new MissingParamError('email')
+      }
+
+      if (!password) {
+        throw new MissingParamError('password')
+      }
+
+      await this.loadUserByEmailRepository.load(email)
     }
+  }
+
+  class LoadUserByEmailRepositorySpy {
+    async load (email) {
+      this.email = email
+    }
+  }
+
+  const loadUserByEmailRepositorySpy = new LoadUserByEmailRepositorySpy()
+  const sut = new AuthUseCase(loadUserByEmailRepositorySpy)
+
+  return {
+    sut,
+    loadUserByEmailRepositorySpy
   }
 }
 
 describe('Auth UseCase', () => {
   test('should throw an error if no email is provided', () => {
-    const sut = new AuthUseCase()
+    const { sut } = makeSut()
     const promise = sut.auth()
 
     expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
 
   test('should throw an error if no password is provided', () => {
-    const sut = new AuthUseCase()
+    const { sut } = makeSut()
     const promise = sut.auth('any_email@mail.com')
 
     expect(promise).rejects.toThrow(new MissingParamError('password'))
+  })
+
+  test('should call LoadUserByEmailRepository with correct email', async () => {
+    const { sut, loadUserByEmailRepositorySpy } = makeSut()
+    await sut.auth('any_email@mail.com', 'any_password')
+
+    expect(loadUserByEmailRepositorySpy.email).toBe('any_email@mail.com')
   })
 })
