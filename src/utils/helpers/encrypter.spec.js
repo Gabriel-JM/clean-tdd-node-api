a784ed2d60ffe0f292761e7fec13af23a6646877
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-06-17 12:52:09 -0300

    Ensure Encrypter returns true if bcrypt returns true

---
+++ src/utils/helpers/encrypter.spec.js
@@ -0,0 +1,19 @@
+
+class Encrypter {
+  async compare (password, hashedPassword) {
+    return true
+  }
+}
+
+const makeSut = () => {
+  return new Encrypter()
+}
+
+describe('Encrypter', () => {
+  test('Should return true if bcrypt returns true', async () => {
+    const sut = makeSut()
+    const isValid = await sut.compare('any_password', 'hashed_password')
+
+    expect(isValid).toBe(true)
+  })
+})
