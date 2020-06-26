80a09322f24e8248cf8b501744544c9b5f7ff5fb
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-06-26 17:22:20 -0300

    Ensure LoadUserByEmailRepository load method returns only the necessary information

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -8,7 +8,14 @@ class LoadUserByEmailRepository {
   }
 
   async load (email) {
-    const user = await this.userModel.findOne({ email })
+    const user = await this.userModel.findOne(
+      { email },
+      {
+        projection: {
+          password: 1
+        }
+      }
+    )
 
     return user
   }
@@ -51,11 +58,15 @@ describe('LoadUserByEmail Repository', () => {
   test('Should return an user if user is found', async () => {
     const { sut, userModel } = makeSut()
 
-    await userModel.insertOne({
-      email: 'valid_email@mail.com'
+    const fromDBUser = await userModel.insertOne({
+      email: 'valid_email@mail.com',
+      password: 'hashed_password'
     })
     const user = await sut.load('valid_email@mail.com')
 
-    expect(user.email).toBe('valid_email@mail.com')
+    expect(user).toEqual({
+      _id: fromDBUser.ops[0]._id,
+      password: fromDBUser.ops[0].password
+    })
   })
 })
