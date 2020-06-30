8e15d657731956f832449abdc2092163dd6826f1
Gabriel-JM <gabriel.josemelo@hotmail.com>
2020-06-30 19:58:22 -0300

    Enable CORS

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -1,11 +1,22 @@
 const request = require('supertest')
 const app = require('./app')
 
+const doNothing = (req, res) => res.send('')
+
 describe('App Setup', () => {
   test('Should disable x-powered-by header', async () => {
-    app.get('/test_x_powered_by', (req, res) => res.send(''))
+    app.get('/test_x_powered_by', doNothing)
     const res = await request(app).get('/test_x_powered_by')
 
     expect(res.headers['x-powered-by']).toBeUndefined()
+  })
+
+  test('Should enable CORS', async () => {
+    app.get('/test_cors', doNothing)
+    const res = await request(app).get('/test_cors')
+
+    expect(res.headers['access-control-allow-origin']).toBe('*')
+    expect(res.headers['access-control-allow-methods']).toBe('*')
+    expect(res.headers['access-control-allow-headers']).toBe('*')
   })
 })
